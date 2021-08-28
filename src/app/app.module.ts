65d901530e267f66c442e442c70d47b3a64f44bf
vubichngan <vubichngan10t@gmail.com>
2021-08-29 00:23:38 +0700

    ujhjh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './component/search/search.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { AdminComponent } from './component/admin/admin.component';
+import { ManageComponent } from './component/manage/manage.component';
+import { UserComponent } from './component/user/user.component';
+import { HomeComponent } from './component/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    LoginComponent,
+    RegisterComponent,
+    AdminComponent,
+    ManageComponent,
+    UserComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
